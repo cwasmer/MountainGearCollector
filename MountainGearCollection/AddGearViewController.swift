ded5b7b06ac283592088484300d4e6c7f4db09df
Christian Wasmer <christian.wasmer@gmail.com>
2017-03-05 13:54:58 +0100

    Add gear items and main list working

--- MountainGearCollection/AddGearViewController.swift
+++ MountainGearCollection/AddGearViewController.swift
@@ -8,30 +8,51 @@
 
 import UIKit
 
-class AddGearViewController: UIViewController {
-
-    @IBOutlet weak var gearImageView: UIImageView!
+class AddGearViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    @IBOutlet weak var gearImageView: UIImageView!
     @IBOutlet weak var itemNameText: UITextField!
+    
+    var imagePicker = UIImagePickerController()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     @IBAction func photosTapped(_ sender: Any) {
+        
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+        
     }
     
-    @IBAction func cameraTapped(_ sender: Any) {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        gearImageView.image = image
+        imagePicker.dismiss(animated: true, completion: nil)
     }
-
+    
     @IBAction func addTapped(_ sender: Any) {
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        let gear = Gear(context: context)
+        gear.title = itemNameText.text
+        gear.image = UIImagePNGRepresentation(gearImageView.image!) as NSData!
+        
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+        navigationController!.popViewController(animated: true)
+        
+        
     }
 }
--- MountainGearCollection/ViewController.swift
+++ MountainGearCollection/ViewController.swift
@@ -8,18 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tavleView: UITableView!
+    
+    var gears : [Gear] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tavleView.dataSource = self
+        tavleView.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        do {
+            gears = try context.fetch(Gear.fetchRequest())
+            tavleView.reloadData()
+        } catch {}
+        
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return gears.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let gear = gears[indexPath.row]
+        cell.textLabel?.text = gear.title
+        cell.imageView?.image = UIImage(data: gear.image! as Data)
+        return cell
+    }
 }
+
 
