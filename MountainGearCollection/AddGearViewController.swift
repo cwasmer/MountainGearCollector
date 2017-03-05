e69ae8a39ddd43a68d4dff3f06b45a499a49451a
Christian Wasmer <christian.wasmer@gmail.com>
2017-03-05 12:31:19 +0100

    Stage 4 complete

---
+++ MountainGearCollection/AddGearViewController.swift
@@ -0,0 +1,37 @@
+//
+//  AddGearViewController.swift
+//  MountainGearCollection
+//
+//  Created by Christian Wasmer on 05/03/2017.
+//  Copyright © 2017 Christian Wasmer. All rights reserved.
+//
+
+import UIKit
+
+class AddGearViewController: UIViewController {
+
+    @IBOutlet weak var gearImageView: UIImageView!
+    
+    @IBOutlet weak var itemNameText: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func photosTapped(_ sender: Any) {
+    }
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+    }
+}
